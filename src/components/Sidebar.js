7c82e0797d71bc1c2b33244c75a7ea21bf46b243
ussyalfaks <usmanalfaki@gmail.com>
2024-07-30 09:13:35 -0700

    synergy dashboard

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Sidebar.css';
-import Avatar from '../assets/avatar.jpeg'
+import Avatar from '../assets/avatar.jpeg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faProjectDiagram, faTasks, faComments, faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
   return (
@@ -8,20 +10,38 @@ const Sidebar = () => {
       <div className="user-info">
         <img src={Avatar} alt="User Avatar" />
         <div>
-        <p>Ussyalfaks</p>
-        <p>Admin</p>
+          <p>Ussyalfaks</p>
+          <p>Admin</p>
         </div>
       </div>
       <nav>
         <ul>
-          <li>Projects manager</li>
-          <li>Pending Synergies</li>
-          <li className="active">Synergies Manager</li>
-          <li>Chat</li>
-          <li>Profile</li>
+          <li>
+            <FontAwesomeIcon icon={faProjectDiagram} />
+            <span>Projects Manager</span>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faTasks} />
+            <span>Pending Synergies</span>
+          </li>
+          <li className="active">
+            <FontAwesomeIcon icon={faTasks} />
+            <span>Synergies Manager</span>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faComments} />
+            <span>Chat</span>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faUser} />
+            <span>Profile</span>
+          </li>
         </ul>
       </nav>
-      <button className="logout-button">Logout</button>
+      <button className="logout-button">
+        <FontAwesomeIcon icon={faSignOutAlt} />
+        Logout
+      </button>
     </div>
   );
 };
