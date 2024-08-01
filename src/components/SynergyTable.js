42902e8ae9d91b9cc692af7c804fe5ec8d6a25c0
ussyalfaks <usmanalfaki@gmail.com>
2024-08-01 09:34:00 -0700

    added images

--- src/components/SynergyTable.js
+++ src/components/SynergyTable.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import SynergyRow from './SynergyRow';
 import './SynergyTable.css';
+import Owner1 from "../assets/3500.jpg"
+import Owner2 from "../assets/3666.jpg"
 
 const synergyData = [
   {
     id: 1,
     name: 'Synergy One',
     creator: 'John Doe',
-    image: 'https://via.placeholder.com/50',
+    image: Owner2,
     price: '$100',
     angels: ['Angel1', 'Angel2'],
     date: '2024-07-30',
@@ -16,7 +18,7 @@ const synergyData = [
     id: 2,
     name: 'Synergy Two',
     creator: 'Jane Smith',
-    image: 'https://via.placeholder.com/50',
+    image: Owner1,
     price: '$200',
     angels: ['Angel3', 'Angel4'],
     date: '2024-07-31',
